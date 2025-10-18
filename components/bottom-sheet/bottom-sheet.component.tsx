3e85edab833ed5346407da3b15683aa268624d9b
matisantillan11 <matias1.santillan@gmail.com>
2025-10-18 19:21:06 -0300

    fix bottom sheet

--- components/bottom-sheet/bottom-sheet.component.tsx
+++ components/bottom-sheet/bottom-sheet.component.tsx
@@ -3,52 +3,27 @@
   BottomSheetModalProps,
   BottomSheetView,
 } from "@gorhom/bottom-sheet"; */
+import { theme } from '@/constants';
+import { BottomSheet as BottomSheetModal } from '@rneui/themed';
 import { useCallback } from 'react';
 
-import { StyleSheet } from 'react-native';
-
-export default function BottomSheet({
-  children,
-  bottomSheetRef,
-  snapPoints = ['50%'],
-  index = 1,
-  ...props
-}: any) {
+export default function BottomSheet({ children, index = 1, items, onClose, ...props }: any) {
   const handleSheetChanges = useCallback((index: number) => {
     console.log('handleSheetChanges', index);
   }, []);
 
-  return <></>;
-}
-
-{
-  /* <BottomSheetModal
+  return (
+    <BottomSheetModal
       {...props}
-      ref={bottomSheetRef}
       onChange={handleSheetChanges}
-      snapPoints={snapPoints}
       index={index}
       containerStyle={{
-        backgroundColor: "#00000075",
-        zIndex: 9999999,
+        paddingBottom: -35,
       }}
-      backgroundStyle={{
-        backgroundColor: theme.primary.brand950,
-      }}
-    >
-      <BottomSheetView style={styles.contentContainer}>
-        {children}
-      </BottomSheetView>
-    </BottomSheetModal> */
+      onBackdropPress={() => onClose()}>
+      {children({
+        styles: { backgroundColor: theme.primary.brand950, padding: 0, zIndex: 9999999 },
+      })}
+    </BottomSheetModal>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contentContainer: {
-    flex: 1,
-    maxHeight: '50%',
-    alignItems: 'center',
-  },
-});
