928cffa2bbe0d52044c376efc6a95fba98501c1c
matisantillan11 <matias1.santillan@gmail.com>
2025-11-06 11:22:20 -0300

    feat (navbar component):
    - Integrate usePathname hook to determine if the current screen is the home screen.
    - Adjust back navigation visibility based on the current screen.
    - Clean up code formatting and standardize quotes for consistency.

--- components/navbar/app-navbar.component.tsx
+++ components/navbar/app-navbar.component.tsx
@@ -3,7 +3,7 @@ import { ROUTES } from "@/constants/routes/routes.constant";
 
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { Image } from "expo-image";
-import { Link } from "expo-router";
+import { Link, usePathname } from "expo-router";
 import { TouchableOpacity } from "react-native";
 import { Appbar, Icon } from "react-native-paper";
 
@@ -13,6 +13,8 @@ type AppNavbarProps = NativeStackHeaderProps & {
 
 export const AppNavbar = (props: AppNavbarProps) => {
   const { handlePresentModalPress } = props;
+  const pathname = usePathname();
+  const isHomeScreen = pathname === "/";
 
   return (
     <Appbar.Header
@@ -20,10 +22,11 @@ export const AppNavbar = (props: AppNavbarProps) => {
       style={{
         backgroundColor: theme.primary.brand800,
         paddingHorizontal: 40,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}>
-      {props.navigation.canGoBack() ? (
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      {props.navigation.canGoBack() && !isHomeScreen ? (
         <TouchableOpacity onPress={() => props.navigation.goBack()}>
           <Icon size={40} source="chevron-left" color="white" />
         </TouchableOpacity>
@@ -34,12 +37,13 @@ export const AppNavbar = (props: AppNavbarProps) => {
             width: 150,
             height: 50,
             marginLeft: -15,
-          }}>
+          }}
+        >
           <Image
-            source={require('../../assets/images/negative-horizontal-logo.png')}
+            source={require("../../assets/images/negative-horizontal-logo.png")}
             style={{
-              width: '100%',
-              height: '100%',
+              width: "100%",
+              height: "100%",
             }}
             contentFit="fill"
             transition={1000}
@@ -47,11 +51,9 @@ export const AppNavbar = (props: AppNavbarProps) => {
         </Link>
       )}
 
-      {props.route.name === 'book/camera' ? null : (
-        <TouchableOpacity onPress={handlePresentModalPress}>
-          <Icon size={35} source="account-circle" color="white" />
-        </TouchableOpacity>
-      )}
+      <TouchableOpacity onPress={handlePresentModalPress}>
+        <Icon size={35} source="account-circle" color="white" />
+      </TouchableOpacity>
     </Appbar.Header>
   );
 };
